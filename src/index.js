446fcb56e94e6d49569e4d60cd7faac48cbd2a92
GarboMuffin <tommyweber33@gmail.com>
2018-12-29 21:44:58 -0600

    Simplify error handling

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const errorhandler = require("errorhandler");
 
 const config = require("./config");
 const logger = require("./logger");
 const app = require("./app");
 
-if (!app.inProduction) {
-  app.use(errorhandler());
-}
-
-if (config.PORT === 80 && app.inProduction) {
-  logger.warn("Listening on port 80 in production? Really?");
-}
+// Small error handler
+app.use((err, req, res, next) => {
+  // `err` might be undefined or null and might not have a `stack`
+  if (err && err.stack) {
+    logger.error(err.stack);
+  } else {
+    logger.error(err);
+  }
+  res.status(500).send("Internal Server Error. Check the logs for more information.");
+});
 
 const server = app.listen(config.PORT, () => {
   const address = server.address();
