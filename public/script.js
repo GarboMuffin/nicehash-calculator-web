87d4dc738d96a999690e2999eaff9dd743c3708a
GarboMuffin <tommyweber33@gmail.com>
2019-02-17 21:46:44 -0600

    Implement dark mode/night mode
     * Add option to toggle dark mode/night mode for entire site
     * Saved on future visits
     * Add <noscript> message
     * Update some text
    
    I wanted there to be a transition between the themes, and there was for
    a little while, but apparently Chrome just sucks at doing transitions so
    I had to scrap it for simplicity. It's not that big of a deal, I'm just
    surprised that Chrome behaved so terrible while IE 11 worked just fine.

--- public/script.js
+++ public/script.js
@@ -1,19 +1,30 @@
 (function() {
   "use strict";
 
-  function convert(t) {
+  /* Convert UTC dates to your local time */
+  function localizeDate(t) {
     return (new Date(t.replace(" (UTC)", ""))).toLocaleString();
   }
-  function fixDates(els, type) {
-    for (var i = 0; i < els.length; i++) {
-      var el = els[i];
-      if (type === "text") {
-        el.textContent = convert(el.textContent);
-      } else if (type === "title") {
-        el.setAttribute("title", convert(el.getAttribute("title")));
-      }
-    }
+  var els = document.getElementsByClassName("utc-text");
+  for (var i = 0; i < els.length; i++) {
+    els[i].textContent = localizeDate(els[i].textContent);
   }
-  fixDates(document.getElementsByClassName("utc-text"), "text");
-  fixDates(document.getElementsByClassName("utc-title"), "title");
+  els = document.getElementsByClassName("utc-title");
+  for (var i = 0; i < els.length; i++) {
+    els[i].setAttribute("title", localizeDate(els[i].getAttribute("title")));
+  }
+
+  /* Support dark mode */
+  document.body.classList.remove('no-js');
+  function toggleDark(transition) {
+    document.body.classList.toggle("dark");
+    localStorage.setItem("userPrefersDark", document.body.classList.contains("dark"));
+  }
+  document.getElementById("dark-mode-container").addEventListener("click", function(e) {
+    toggleDark(true);
+    e.preventDefault();
+  });
+  if (localStorage.getItem("userPrefersDark") === "true") {
+    toggleDark(false);
+  }
 }());
